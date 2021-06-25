f6496c428bef2dc0191a2e2350c7253ad51b9fec
Ildar Karimov <thevery@yandex-team.ru>
2021-06-25 11:59:10 +0300

    Initial commit

---
+++ Sample/SampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SampleApp.swift
+//  Sample
+//
+//  Created by Ildar Karimov on 25.06.2021.
+//
+
+import SwiftUI
+
+@main
+struct SampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
